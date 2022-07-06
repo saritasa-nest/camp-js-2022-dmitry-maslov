fb85c776b3ea6ae04187a7ce2d3ecb6147fc70b6
dmitriqm <1dmitriqm@gmail.com>
2022-07-06 14:22:19 +0500

    created anime table element component

---
+++ apps/vanilla/src/components/animeTableElement.ts
@@ -0,0 +1,30 @@
+import { ListAnime } from '@js-camp/core/models/listAnime';
+
+import { $, Dom } from '../core/Dom';
+
+/**
+ * Creates the HOME element of the anime table.
+ * @param listAnime ListAnime Object.
+ * @returns Instance HOUSE describing element of anime table.
+ */
+export function $createAnimeTableElement(listAnime: ListAnime): Dom {
+
+  const $image = $.create<HTMLImageElement>('img', 'w-40 flex-shrink-0 flex-grow-1 justify-between');
+  $image.$el.src = listAnime.image;
+  const $imageCol = $.create('td', 'border-r').append($image);
+
+  const $engTitle = $.create('span').setTextContent(listAnime.titleEng);
+  const $titleCol = $.create('td', 'border-r').append($engTitle);
+
+  const $status = $.create('span').setTextContent(listAnime.status);
+  const $statusCol = $.create('td', 'border-r').append($status);
+
+  const $airedStart = $.create('span').setTextContent(String(listAnime.airedStart ? listAnime.airedStart.getFullYear() : 'Unknown'));
+  const $airedStartCol = $.create('td', 'border-r').append($airedStart);
+
+  return $.create('tr', 'flex border-y my-1 w-full justify-between')
+    .append($imageCol)
+    .append($titleCol)
+    .append($statusCol)
+    .append($airedStartCol);
+}
