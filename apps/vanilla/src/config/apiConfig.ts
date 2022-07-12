1cfe0fc42918135197afd40da933d4c4d74ac97b
dmitriqm <1dmitriqm@gmail.com>
2022-07-12 16:08:12 +0500

    fix: ts-lint fix

--- apps/vanilla/src/config/apiConfig.ts
+++ apps/vanilla/src/config/apiConfig.ts
@@ -3,6 +3,7 @@ import { AxiosRequestConfig } from 'axios';
 export const apiConfig: AxiosRequestConfig = {
   baseURL: import.meta.env.VITE_API_URL,
   headers: {
+    // eslint-disable-next-line @typescript-eslint/naming-convention
     'Api-Key': import.meta.env.VITE_API_KEY,
   },
 };
--- apps/vanilla/src/vite-env.d.ts
+++ apps/vanilla/src/vite-env.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/naming-convention */
 
 // eslint-disable-next-line spaced-comment
 /// <reference types="vite/client" />
