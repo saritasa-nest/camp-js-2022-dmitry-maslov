61e5bfe143fa5e3f4f5c1874faa4acdd613f1b5d
dmitriqm <1dmitriqm@gmail.com>
2022-07-18 15:28:57 +0500

    Add doc to anime filter interface #JC19-367

--- libs/core/interfaces/filter.ts
+++ libs/core/interfaces/filter.ts
@@ -2,8 +2,9 @@ import { AnimeType } from '../enums/anime/type';
 
 import { AnimeFilterType } from './../enums/anime/filters';
 
+/** Anime Filters interface. */
 export interface AnimeFilters {
 
-  /**  */
+  /** Type anime. */
   [AnimeFilterType.Type]: AnimeType[];
 }
