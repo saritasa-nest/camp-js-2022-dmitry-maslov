40fd747165ca697e25a1d78324f5e0b3fcd59778
dmitriqm <1dmitriqm@gmail.com>
2022-07-18 16:21:17 +0500

    Change enums value from SCREAMING_SNAKE_CASE to PascalCase #JC19-332

--- libs/core/mappers/pagination.mapper.ts
+++ libs/core/mappers/pagination.mapper.ts
@@ -5,9 +5,10 @@ import { PaginationDto } from './../dtos/pagination.dto';
 export namespace PaginationMapper {
 
   /**
-   * FromDTO.
-   * @param resultMapper ResultMapper.
-   * @param dto Dto.
+   * Converted pagination dto to pagination model.
+   * @param resultMapper Mapper function that converted the DTO
+  result to model result.
+   * @param dto Pagination dto.
    */
   export function fromDto<T, TDto>(resultMapper: (resultDto: TDto) => T, dto: PaginationDto<TDto>): Pagination<T> {
     return new Pagination<T>({
