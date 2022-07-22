91f67ac7e27546559808481d69df2543b4159080
dmitriqm <1dmitriqm@gmail.com>
2022-07-22 16:53:52 +0500

    Fix imports #JC19-398

--- apps/angular/src/app/app.module.ts
+++ apps/angular/src/app/app.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { ApiKeyInterceptor } from '../core/interceptors/api-key-interceptor';
 
 import { SharedModule } from './../shared/shared.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 const httpInterceptorProviders = [
   {
