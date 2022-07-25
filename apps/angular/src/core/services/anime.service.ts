b5639f56f44bd3e1c756efd48d5164cafbe31715
dmitriqm <1dmitriqm@gmail.com>
2022-07-25 10:53:50 +0500

    Fix imports #JC19-398

--- apps/angular/src/core/services/anime.service.ts
+++ apps/angular/src/core/services/anime.service.ts
@@ -1,13 +1,10 @@
+import { map, Observable } from 'rxjs';
 import { AnimeDTO } from '@js-camp/core/dtos/anime.dto';
 import { AnimeMapper } from '@js-camp/core/mappers/anime.mapper';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
-import { map, Observable } from 'rxjs';
 import { Anime } from '@js-camp/core/models/anime/anime';
-
 import { PaginationDto } from '@js-camp/core/dtos/pagination.dto';
-
 import { PaginationMapper } from '@js-camp/core/mappers/pagination.mapper';
 
 import { AppConfigService } from './app-config.service';
