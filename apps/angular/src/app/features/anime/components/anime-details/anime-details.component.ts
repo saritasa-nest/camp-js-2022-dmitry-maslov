572fe5f75b0eec4c9557d764a7c4dcf8dd4c002c
dmitriqm <1dmitriqm@gmail.com>
2022-08-09 17:29:52 +0500

    Add anime status and anime type readable view in components #JC19-440

--- apps/angular/src/app/features/anime/components/anime-details/anime-details.component.ts
+++ apps/angular/src/app/features/anime/components/anime-details/anime-details.component.ts
@@ -1,6 +1,8 @@
 import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { AnimeService } from '@js-camp/angular/core/services/anime.service';
 import { Anime } from '@js-camp/core/models/anime';
+import { AnimeStatus } from '@js-camp/core/models/anime-status';
+import { AnimeType } from '@js-camp/core/models/anime-type';
 import { Observable } from 'rxjs';
 
 /** Anime Detail Component. */
@@ -11,6 +13,12 @@ import { Observable } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class AnimeDetailsComponent implements OnInit {
+
+  /** Anime status. */
+  public readonly animeStatus = AnimeStatus;
+
+  /** Anime type. */
+  public readonly animeType = AnimeType;
 
   /** Anime id. */
   @Input()
