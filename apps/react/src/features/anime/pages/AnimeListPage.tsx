335f0aef7ad111c20d88b4602e3591cb5fc1d991
Dmtiry Maslov <1dmitriqm@gmail.com>
2022-08-22 15:49:18 +0500

    Add list page #JC19-470

--- apps/react/src/features/anime/pages/AnimeListPage.tsx
+++ apps/react/src/features/anime/pages/AnimeListPage.tsx
@@ -1,7 +1,18 @@
+import { Grid } from '@mui/material';
 import { FC, memo } from 'react';
+import { Outlet } from 'react-router-dom';
 
 import { AnimeList } from '../components/AnimeList/AnimeList';
 
-const AnimeListPageComponent: FC = () => (<AnimeList></AnimeList>);
+const AnimePageComponent: FC = () => (
+  <Grid container>
+    <Grid item xs={3}>
+      <AnimeList></AnimeList>
+    </Grid>
+    <Grid item xs={12}>
+      <Outlet />
+    </Grid>
+  </Grid>
+);
 
-export const AnimeListPage = memo(AnimeListPageComponent);
+export const AnimePage = memo(AnimePageComponent);
